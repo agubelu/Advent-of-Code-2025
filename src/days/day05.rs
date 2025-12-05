b9afccce9f0ed4d48ac34ac13e8e32a372cd23f9
agubelu <agustinbd3@gmail.com>
2025-12-05 14:45:09 +0100

    🛒 Day 05 🛒

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -15,7 +15,7 @@ pub fn solve() -> SolutionPair {
     let input = read_to_string("input/day05.txt").unwrap();
     let (ranges, ids) = parse_input(&input);
 
-    let sol1 = ids.iter().filter(|&&x| is_fresh(x, &ranges)).count();
+    let sol1 = ids.into_iter().filter(|&x| is_fresh(x, &ranges)).count();
     let sol2 = count_common_values(&ranges);
 
     (Solution::from(sol1), Solution::from(sol2))
@@ -25,21 +25,10 @@ fn is_fresh(id: u64, ranges: &[Range]) -> bool {
     ranges.iter().any(|&(min, max)| min <= id && id <= max)
 }
 
-fn flatten_ranges(ranges: &[Range]) -> Vec<(u64, Marker)> {
-    let mut res = vec![];
-
-    for &(min, max) in ranges {
-        res.push((min, Start));
-        res.push((max + 1, End));
-    }
-
-    res.sort_by_key(|x| x.0);
-    res
-}
-
 fn count_common_values(ranges: &[Range]) -> u64 {
     let (mut start, mut active, mut total) = (0, 0, 0);
-    let flat = flatten_ranges(ranges);
+    let mut flat = ranges.iter().flat_map(|&(min, max)| [(min, Start), (max + 1, End)]).collect_vec();
+    flat.sort_by_key(|x| x.0);
 
     for (val, marker) in flat {
         if marker == Start {
